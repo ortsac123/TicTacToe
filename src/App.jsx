f9795ef93b846e2ff36dd081c148bb99504abc7d
rafael <rafakastro27@gmail.com>
2025-10-14 16:56:42 -0500

    Primer avance del juego TicTacToe

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+
+
+const Square = ({children, updateBoard, index, isSelected})=>{
+
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick =() =>{
+    updateBoard(index)
+  }
+
+  return(
+    <div onClick={handleClick} className={className}>{children}</div> 
+  )
+}
+
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+  const chackWinner = (boardToCheck)=>{
+    for(const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] == boardToCheck[b] && 
+        boardToCheck[a] == boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const updateBoard = (index)=>{
+    
+    //Evitar sobreescribir 
+    if (board[index] || winner) return
+
+    //Actualizar tablero 
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newWinner = chackWinner(newBoard)
+
+    if (newWinner){
+      setWinner(newWinner)
+      alert(newWinner)
+    }
+
+    const newTurn = turn == TURNS.O ? TURNS.X : TURNS.O
+    setTurn(newTurn)
+  }
+
+  return (  
+    <main className='board'>
+      <h1>Tic-Tac-Toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index)=>{
+            return(
+             <Square 
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+             >{board[index]}</Square>
+            )
+          })
+        }
+      </section>
+      <section className='turn'>
+        <Square isSelected = {turn == TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected = {turn == TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  )
+}
+
+export default App
